1aaca3c5a03870affce935f8ed10c9183902e254
SHravankumar Muchchandi <shravankumarmuchchandi@gmail.com>
2025-09-24 20:51:36 +0530

    Updated resume

--- src/public class Main {.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        var name = "Ajay";
-        var percentage =  98.5;
-        var calssroom = 10;
-        System.out.println("Student + Ajay + achieved 98.5")
-    }
-}
